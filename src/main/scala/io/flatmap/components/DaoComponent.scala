6172ad7b8add0783e8c73316ea5b21fface1b11a
Shokunin San <me@flatmap.io>
2014-07-13 02:11:49 +0200

    First draft of bookmark storage.

--- src/main/scala/io/flatmap/components/DaoComponent.scala
+++ src/main/scala/io/flatmap/components/DaoComponent.scala
@@ -3,6 +3,7 @@ package io.flatmap.components
 import io.flatmap.models.Bookmark
 import scala.pickling._
 import json._
+import scala.reflect.io.{Path, File, Directory}
 
 trait DaoComponent {
 
@@ -18,7 +19,13 @@ trait DaoComponentImpl extends DaoComponent {
 
   class DaoImpl extends Dao {
 
-    def write(bookmark: Bookmark): Unit = println(bookmark.pickle)
+    def write(bookmark: Bookmark): Unit = {
+      val home = System.getenv("HOME")
+      val filename = System.currentTimeMillis().toString
+      val path = Path(s"$home/.bookmark/$filename")
+      val file = path.createFile()
+      file.writeAll(bookmark.pickle.toString)
+    }
 
   }
 
